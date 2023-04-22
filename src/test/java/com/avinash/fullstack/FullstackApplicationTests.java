133a6fbd4bb7e7bbd77d6026b4390a6816222418
Avinash Kumar <avinash.kumar@cognizer.ai>
2023-04-23 00:15:55 +0530

    sample test case

--- src/test/java/com/avinash/fullstack/FullstackApplicationTests.java
+++ src/test/java/com/avinash/fullstack/FullstackApplicationTests.java
@@ -1,13 +1,26 @@
 package com.avinash.fullstack;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class FullstackApplicationTests {
 
+	@Tag("slow")
 	@Test
-	void contextLoads() {
+	 void testAdd() {
+		assertEquals(42, Integer.sum(19, 23));
+	}
+	@Tag("fast")
+	@Test
+	 void testDivide() {
+		assertThrows(ArithmeticException.class, () -> {
+			Integer.divideUnsigned(42, 0);
+		});
 	}
 
 }
